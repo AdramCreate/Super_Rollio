9c40ee0a601a3b4082cb6f435d3a260b2dc09908
Adrian S <adrian.r.sanpedro@gmail.com>
2018-09-17 14:58:13 -0700

    Added Player, Initial Scripts
    Added Sphere Player, Scripts for Player and Camera

---
+++ Super Rollio/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	// Public Members
+	public GameObject target;
+
+	// Private Members
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = target.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Best update to call after all physics calculations are done
+	void LateUpdate(){
+		transform.position = target.transform.position - offset;
+		transform.LookAt(target.transform);
+	}
+}
---
+++ Super Rollio/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	// Public Members
+	public int speed;
+	
+	// Private Members
+	private RigidBody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// For physics calculations
+	void FixedUpdate(){
+		float moveWS = Input.GetAxis("Horizontal");
+		float moveAD = Input.GetAxis("Vertical");
+		Vector3 newMovement = new Vector3(moveWS, moveAD);
+		rb.AddForce(newMovement * speed);
+	}
+}
